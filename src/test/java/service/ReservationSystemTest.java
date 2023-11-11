86d895cbdb4d4a0072b30e3f449c32df89b3b20c
urquijo1234 <josephcamilourquijo@gmail.com>
2023-11-11 13:27:15 -0500

    added unit tests

--- src/test/java/service/ReservationSystemTest.java
+++ src/test/java/service/ReservationSystemTest.java
@@ -17,12 +17,23 @@ public class ReservationSystemTest {
 
     @Test
     public void testReserveRoom() {
-        // Implementar pruebas aquí
+        // Habitación válida
+        assertEquals(true, system.reserveRoom("A1"));
+        // Intentar reservar la misma habitación nuevamente (debería devolver falso)
+        assertEquals(false, system.reserveRoom("A1"));
+        // Intentar reservar una habitación inexistente (debería devolver falso)
+        assertEquals(false, system.reserveRoom("B1"));
     }
 
     @Test
     public void testCancelReservation() {
-        // Implementar pruebas aquí
+        // Intentar cancelar la reserva de una habitación no reservada (debería devolver falso)
+        assertEquals(false, system.cancelReservation("A1"));
+        // Reservar una habitación y luego cancelar la reserva (debería devolver true)
+        system.reserveRoom("A1");
+        assertEquals(true, system.cancelReservation("A1"));
+        // Intentar cancelar la reserva de una habitación inexistente (debería devolver falso)
+        assertEquals(false, system.cancelReservation("B1"));
     }
 
     // Puedes añadir más pruebas según lo consideres necesario

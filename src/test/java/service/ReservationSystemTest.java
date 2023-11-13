b8eebe3fd20e2fc47cf4cdc7f984620d7c2f4d89
Sofia Santis <sofia.santis2@aiesec.net>
2023-11-13 18:17:06 -0500

    Pruebas unitarias con Mockito y nueva rama sof_Pruebas

--- src/test/java/service/ReservationSystemTest.java
+++ src/test/java/service/ReservationSystemTest.java
@@ -1,29 +1,51 @@
 package service;
 
+import dao.ConferenceRoomDao;
 import model.ConferenceRoom;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 import static org.junit.jupiter.api.Assertions.*;
-
+import static org.junit.jupiter.api.Assertions.*;
 public class ReservationSystemTest {
 
     private ReservationSystem system;
-
+    private ConferenceRoomDao conferenceRoomMock;
     @BeforeEach
     public void setUp() {
-        //system = new ReservationSystem();
+        conferenceRoomMock = mock(ConferenceRoomDao.class);
+        system = new ReservationSystem(conferenceRoomMock);
     }
 
     @Test
-    public void testReserveRoom() {
-        // Implementar pruebas aquí
+    public void testReserveRoomWhenIsFalse() {
+        when(conferenceRoomMock.getRoomById("romm1")).thenReturn(null);
+        assertEquals(false, system.reserveRoom("room1"));
+    }
+    @Test
+    public void testReserveRoomWhenIsTrue() {
+        ConferenceRoom prueba2 = new ConferenceRoom("room2", 5);
+        when(conferenceRoomMock.getRoomById("room2")).thenReturn(prueba2);
+        assertEquals(true, system.reserveRoom("room2"));
     }
 
     @Test
-    public void testCancelReservation() {
-        // Implementar pruebas aquí
+    public void testCancelReservationWhenIsFalse() {
+        when(conferenceRoomMock.getRoomById("romm3")).thenReturn(null);
+        assertEquals(false, system.cancelReservation("room3"));
     }
 
-    // Puedes añadir más pruebas según lo consideres necesario
+
+    @Test
+    public void testCancelReservationWhenIsTrue() {
+        ConferenceRoom prueba3 = new ConferenceRoom("room4", 5);
+        prueba3.setReserved(true);
+        when(conferenceRoomMock.getRoomById("room4")).thenReturn(prueba3);
+        assertEquals(true, system.cancelReservation("room4"));
+
+
+    }
 }
